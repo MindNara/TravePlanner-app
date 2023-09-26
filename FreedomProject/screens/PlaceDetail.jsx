cc19820b25a66ffa1b03fed73b39d33fbc554bff
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2023-09-26 18:09:20 +0700

    edit scollView

--- FreedomProject/screens/PlaceDetail.jsx
+++ FreedomProject/screens/PlaceDetail.jsx
@@ -6,9 +6,11 @@ import {
     StyleSheet,
     Image,
     Pressable,
-    ImageBackground
+    ImageBackground,
+    ScrollView
 } from 'react-native';
 import { useFonts } from '@expo-google-fonts/prompt';
+// import { ScrollView } from 'react-native-gesture-handler';
 
 export default function PlaceDetail({ navigation }) {
 
@@ -26,48 +28,51 @@ export default function PlaceDetail({ navigation }) {
 
     return (
         <SafeAreaView className="container mx-auto h-full bg-white">
-            {/* Header & Image */}
-            <View className="relative w-full h-[420px] bg-blue-light">
-                {/* <Image className="absolute w-full h-full" source={require('../assets/IntroImage.png')} /> */}
-                <View className="mx-[32px] pt-16 flex flex-row justify-between">
-                    <Pressable onPress={() => {
-                        navigation.goBack();
-                    }}>
-                        <View className="relative justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
-                        <Image className="absolute top-[6.5px] left-[5px]" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/back.png' }}
-                            style={{ width: 22, height: 22 }} />
-                    </Pressable>
-                    <View>
-                        <View className="relative justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
-                        <Image className="absolute top-[9px] left-2" source={{ uri: 'https://img.icons8.com/material-outlined/96/2E2E2E/filled-like.png' }}
-                            style={{ width: 20, height: 20 }} />
-                    </View>
-                </View>
-            </View>
-            {/* Content */}
-            <View className="absolute bottom-0 bg-white w-full h-[500px] rounded-t-[50px]">
-                <View className="mx-[32px] pt-16">
-                    <View>
-                        <Text className="text-[16px] text-gray-dark" style={{ fontFamily: 'promptRegular' }}>PROVINCE</Text>
-                        <Text className="text-[32px] text-gray-dark mt-[-6px]" style={{ fontFamily: 'promptSemiBold' }}>Place Name</Text>
-                        <Text className="text-[13px] text-gray-dark mt-[20px] leading-4" style={{ fontFamily: 'promptLight' }}>Lorem ipsum dolor sit amet, consectetur adipiscing eli, sed do eiusmod tempor incididunt ut labore et dolore magna elit, sed do eiusmod tempor incididunt ut labor et dolore magna eiusmod tempor eopo.</Text>
-                    </View>
-                    <View className="flex flex-row mt-[30px] items-center gap-x-[16px]">
-                        <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
-                            <Image className="" source={{ uri: 'https://img.icons8.com/pastel-glyph/64/2E2E2E/shipping-location--v1.png' }}
+            <ScrollView>
+                {/* Header & Image */}
+                <View className="w-full h-full bg-blue-light">
+                    {/* <Image className="absolute w-full h-full" source={require('../assets/IntroImage.png')} /> */}
+                    <View className="mx-[32px] pt-16 flex flex-row justify-between">
+                        <Pressable onPress={() => {
+                            navigation.goBack();
+                        }}>
+                            <View className="relative justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
+                            <Image className="absolute top-[6.5px] left-[5px]" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/back.png' }}
+                                style={{ width: 22, height: 22 }} />
+                        </Pressable>
+                        <View>
+                            <View className="relative justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
+                            <Image className="absolute top-[9px] left-2" source={{ uri: 'https://img.icons8.com/material-outlined/96/2E2E2E/filled-like.png' }}
                                 style={{ width: 20, height: 20 }} />
                         </View>
-                        <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>LOCATION TYPE</Text>
                     </View>
-                    <View className="flex flex-row mt-[10px] items-center gap-x-[16px]">
-                        <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
-                            <Image className="" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/2E2E2E/map-marker.png' }}
-                                style={{ width: 22, height: 22 }} />
+
+                    {/* content */}
+                    <View className="bottom-0 mt-40 bg-white w-full h-full rounded-t-[50px]">
+                        <View className="mx-[32px] pt-16">
+                            <View>
+                                <Text className="text-[16px] text-gray-dark" style={{ fontFamily: 'promptRegular' }}>PROVINCE</Text>
+                                <Text className="text-[32px] text-gray-dark mt-[-6px]" style={{ fontFamily: 'promptSemiBold' }}>Place Name</Text>
+                                <Text className="text-[13px] text-gray-dark mt-[20px] leading-4" style={{ fontFamily: 'promptLight' }}>Lorem ipsum dolor sit amet, consectetur adipiscing eli, sed do eiusmod tempor incididunt ut labore et dolore magna elit, sed do eiusmod tempor incididunt ut labor et dolore magna eiusmod tempor eopo.</Text>
+                            </View>
+                            <View className="flex flex-row mt-[30px] items-center gap-x-[16px]">
+                                <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
+                                    <Image className="" source={{ uri: 'https://img.icons8.com/pastel-glyph/64/2E2E2E/shipping-location--v1.png' }}
+                                        style={{ width: 20, height: 20 }} />
+                                </View>
+                                <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>LOCATION TYPE</Text>
+                            </View>
+                            <View className="flex flex-row mt-[10px] items-center gap-x-[16px]">
+                                <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
+                                    <Image className="" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/2E2E2E/map-marker.png' }}
+                                        style={{ width: 22, height: 22 }} />
+                                </View>
+                                <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>VIEW LOCATION</Text>
+                            </View>
                         </View>
-                        <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>VIEW LOCATION</Text>
                     </View>
                 </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
--- FreedomProject/screens/TripDetail.jsx
+++ FreedomProject/screens/TripDetail.jsx
@@ -27,82 +27,85 @@ export default function TripDetail({ navigation }) {
     }
 
     return (
-        <SafeAreaView className="relative container mx-auto h-full bg-white">
-            {/* Header & Image */}
-            <View className="w-full h-[420px] bg-blue-light">
-                {/* <Image className="absolute w-full h-full" source={require('../assets/IntroImage.png')} /> */}
-                <View className="mx-[32px] pt-16 flex flex-row justify-between">
-                    <Pressable onPress={() => {
-                        navigation.goBack();
-                    }}>
-                        <View className="justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
-                        <Image className="absolute top-[6.5px] left-[5px]" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/back.png' }}
-                            style={{ width: 22, height: 22 }} />
-                    </Pressable>
-                    <View>
-                        <View className="justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
-                        <Image className="absolute top-[9px] left-2" source={{ uri: 'https://img.icons8.com/material-outlined/96/2E2E2E/filled-like.png' }}
-                            style={{ width: 20, height: 20 }} />
+        <SafeAreaView className="container mx-auto h-full bg-white">
+            <ScrollView>
+                {/* Header & Image */}
+                <View className="w-full h-full bg-blue-light">
+                    {/* <Image className="absolute w-full h-full" source={require('../assets/IntroImage.png')} /> */}
+                    <View className="mx-[32px] pt-16 flex flex-row justify-between">
+                        <Pressable onPress={() => {
+                            navigation.goBack();
+                        }}>
+                            <View className="justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
+                            <Image className="absolute top-[6.5px] left-[5px]" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/back.png' }}
+                                style={{ width: 22, height: 22 }} />
+                        </Pressable>
+                        <View>
+                            <View className="justify-center items-center h-[36px] w-[36px] bg-white rounded-3xl opacity-50"></View>
+                            <Image className="absolute top-[9px] left-2" source={{ uri: 'https://img.icons8.com/material-outlined/96/2E2E2E/filled-like.png' }}
+                                style={{ width: 20, height: 20 }} />
+                        </View>
+                    </View>
+
+                    {/* Content */}
+                    <View className="bg-white bottom-0 mt-40 w-full h-full rounded-t-[50px]">
+
+                        <View className="mx-[32px] pt-12">
+                            <View className="flex flex-row items-center">
+                                <View style={[styles.textbox, { alignItems: 'center', justifyContent: 'center' }]}>
+                                    <Text className="text-[20px] text-white" style={{ fontFamily: 'promptBold' }}>03</Text>
+                                    <Text className="text-[12px] text-white mt-[-5px]" style={{ fontFamily: 'promptBold' }}>DAY</Text>
+                                </View>
+                                <Text className="text-[26px] text-gray-dark ml-[20px]" style={{ fontFamily: 'promptSemiBold' }}>Trip Name</Text>
+                            </View>
+
+                            <View className="pt-[30px] ml-[30px] mr-[10px]">
+
+                                {/* box day1 */}
+                                <View className="flex flex-row items-center mb-[15px]">
+                                    <View className="w-[24px] h-[24px] left-[-10] rounded-xl items-center justify-center border-collapse border-[1px]">
+                                        <View className="bg-gray-dark w-[16px] h-[16px] rounded-xl"></View>
+                                    </View>
+                                    <Text className="text-[20px] ml-[5px] text-gray-dark" style={{ fontFamily: 'promptBold' }}>DAY 1</Text>
+                                </View>
+                                <View className="h-auto border-dashed border-l-2 border-black ml-[1px]">
+                                    <View style={{ alignItems: "center", justifyContent: "center" }}>
+                                        <TripOnDay navigation={navigation}></TripOnDay>
+                                    </View>
+                                </View>
+
+                                {/* box day2 */}
+                                <View className="flex flex-row items-center my-[15px]">
+                                    <View className="w-[24px] h-[24px] left-[-10] rounded-xl items-center justify-center border-collapse border-[1px]">
+                                        <View className="bg-gray-dark w-[16px] h-[16px] rounded-xl"></View>
+                                    </View>
+                                    <Text className="text-[20px] ml-[5px] text-gray-dark" style={{ fontFamily: 'promptBold' }}>DAY 2</Text>
+                                </View>
+                                <View className="h-auto border-dashed border-l-2 border-black ml-[1px]">
+                                    <View style={{ alignItems: "center", justifyContent: "center" }}>
+                                        <TripOnDay navigation={navigation}></TripOnDay>
+                                    </View>
+                                </View>
+
+                                {/* box day3 */}
+                                <View className="flex flex-row items-center my-[15px]">
+                                    <View className="w-[24px] h-[24px] left-[-10] rounded-xl items-center justify-center border-collapse border-[1px]">
+                                        <View className="bg-gray-dark w-[16px] h-[16px] rounded-xl"></View>
+                                    </View>
+                                    <Text className="text-[20px] ml-[5px] text-gray-dark" style={{ fontFamily: 'promptBold' }}>DAY 3</Text>
+                                </View>
+                                <View className="h-auto border-dashed border-l-2 border-black ml-[1px]">
+                                    <View style={{ alignItems: "center", justifyContent: "center" }}>
+                                        <TripOnDay navigation={navigation}></TripOnDay>
+                                    </View>
+                                </View>
+                                
+
+                            </View>
+                        </View>
                     </View>
                 </View>
-            </View>
-
-            {/* Content */}
-            <View className="absolute bottom-0 bg-white w-full h-[720px] rounded-t-[50px]">
-
-                <View className="mx-[32px] pt-12">
-                    <View className="flex flex-row items-center">
-                        <View style={[styles.textbox, { alignItems: 'center', justifyContent: 'center' }]}>
-                            <Text className="text-[20px] text-white" style={{ fontFamily: 'promptBold' }}>03</Text>
-                            <Text className="text-[12px] text-white mt-[-5px]" style={{ fontFamily: 'promptBold' }}>DAY</Text>
-                        </View>
-                        <Text className="text-[26px] text-gray-dark ml-[20px]" style={{ fontFamily: 'promptSemiBold' }}>Trip Name</Text>
-                    </View>
-
-                    <View className="pt-[30px] ml-[30px] mr-[10px]">
-
-                        {/* box day1 */}
-                        <View className="flex flex-row items-center mb-[15px]">
-                            <View className="w-[24px] h-[24px] left-[-10] rounded-xl items-center justify-center border-collapse border-[1px]">
-                                <View className="bg-gray-dark w-[16px] h-[16px] rounded-xl"></View>
-                            </View>
-                            <Text className="text-[20px] ml-[5px] text-gray-dark" style={{ fontFamily: 'promptBold' }}>DAY 1</Text>
-                        </View>
-                        <View className="h-auto border-dashed border-l-2 border-black ml-[1px]">
-                            <View style={{ alignItems: "center", justifyContent: "center" }}>
-                                <TripOnDay navigation={navigation}></TripOnDay>
-                            </View>
-                        </View>
-
-                        {/* box day2 */}
-                        <View className="flex flex-row items-center my-[15px]">
-                            <View className="w-[24px] h-[24px] left-[-10] rounded-xl items-center justify-center border-collapse border-[1px]">
-                                <View className="bg-gray-dark w-[16px] h-[16px] rounded-xl"></View>
-                            </View>
-                            <Text className="text-[20px] ml-[5px] text-gray-dark" style={{ fontFamily: 'promptBold' }}>DAY 2</Text>
-                        </View>
-                        <View className="h-auto border-dashed border-l-2 border-black ml-[1px]">
-                            <View style={{ alignItems: "center", justifyContent: "center" }}>
-                                <TripOnDay navigation={navigation}></TripOnDay>
-                            </View>
-                        </View>
-
-                        {/* box day3 */}
-                        <View className="flex flex-row items-center my-[15px]">
-                            <View className="w-[24px] h-[24px] left-[-10] rounded-xl items-center justify-center border-collapse border-[1px]">
-                                <View className="bg-gray-dark w-[16px] h-[16px] rounded-xl"></View>
-                            </View>
-                            <Text className="text-[20px] ml-[5px] text-gray-dark" style={{ fontFamily: 'promptBold' }}>DAY 3</Text>
-                        </View>
-                        <View className="h-auto border-dashed border-l-2 border-black ml-[1px]">
-                            <View style={{ alignItems: "center", justifyContent: "center" }}>
-                                <TripOnDay navigation={navigation}></TripOnDay>
-                            </View>
-                        </View>
-
-                    </View>
-                </View>
-            </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
