db04ff91bb7c5076a3203a860d7de0e1c170e28b
MindNara <88419813+MindNara@users.noreply.github.com>
2023-10-10 21:18:36 +0700

    add backend (node.js) config

---
+++ FreedomProject/server/routes/trip.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const pool = require("../config");
+
+const router = express.Router();
+
+router.get('/trip', async function (req, res) {
+
+});
+
+router.post('/trip', async function (req, res) {
+
+});
+
+router.delete('/trip', async function (req, res) {
+
+});
+
+exports.router = router;
