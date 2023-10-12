8c88615bab0d03dd0b1b92369cb50c09068bea37
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2023-10-12 12:15:09 +0700

    edit fetch detailPlace

--- FreedomProject/app/src/screens/PlaceDetail.jsx
+++ FreedomProject/app/src/screens/PlaceDetail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     SafeAreaView,
     Text,
@@ -12,10 +12,33 @@ import {
 import { useFonts } from '@expo-google-fonts/prompt';
 // import { ScrollView } from 'react-native-gesture-handler';
 
-export default function PlaceDetail({ route, navigation }) {
+export default function PlaceDetails({ route, navigation }) {
 
     const item = route.params.item;
-    // console.log(item);
+    console.log(item);
+
+    const [loading, setLoading] = useState(true);
+    const [dataDetail, setDataDetail] = useState([]);
+
+    const apiKey = 'GBlAR1kAdZLNcsEPOzvbb6chWCSSoyX2qORdP5ifIdceDVTo2crn)n0yJHoUqvj4V=2';
+
+    useEffect(() => {
+        fetch('https://tatapi.tourismthailand.org/tatapi/v5/' + item.category_code + '/' + item.place_id, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`,
+                "Accept-Language": "TH"
+            },
+        })
+            .then((response) => response.json())
+            .then((json) => {
+                setDataDetail(json);
+                setLoading(false);
+            })
+            .catch((error) => console.error(error));
+    }, []);
+
+    console.log("eiei");
+    console.log(dataDetail);
 
     const [loaded] = useFonts({
         promptLight: require("../assets/fonts/Prompt-Light.ttf"),
@@ -55,25 +78,28 @@ export default function PlaceDetail({ route, navigation }) {
                     {/* content */}
                     <View className="bottom-0 mt-40 bg-white w-full h-full rounded-t-[50px]">
                         <View className="mx-[32px] pt-16">
-                            <Text className="text-[16px] text-gray-dark" style={{ fontFamily: 'promptRegular' }}>{item.location.province}</Text>
-                            <Text className="text-[28px] text-gray-dark mt-[-6px]" style={{ fontFamily: 'promptSemiBold' }}>{item.place_name}</Text>
-                            {item.sha.sha_type_description != null && (
-                                <Text className="text-[13px] text-gray-dark mt-[20px]" style={{ fontFamily: 'promptLight' }}>{item.sha.sha_type_description}</Text>
-                            )}
-                            <View className="flex flex-row items-center mt-[30px] gap-x-[16px]">
-                                <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
-                                    <Image className="" source={{ uri: 'https://img.icons8.com/pastel-glyph/64/2E2E2E/shipping-location--v1.png' }}
-                                        style={{ width: 20, height: 20 }} />
-                                </View>
-                                <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>{item.category_code} ({item.category_description})</Text>
-                            </View>
-                            <View className="flex flex-row mt-[10px] items-center gap-x-[16px]">
-                                <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
-                                    <Image className="" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/2E2E2E/map-marker.png' }}
-                                        style={{ width: 22, height: 22 }} />
-                                </View>
-                                <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>VIEW LOCATION</Text>
-                            </View>
+                            {loading || !dataDetail.result ?
+                                (<Text>Loading...</Text>) : (
+                                    <View>
+                                        <Text className="text-[16px] text-gray-dark" style={{ fontFamily: 'promptRegular' }}>{dataDetail.result.destination}</Text>
+                                        <Text className="text-[28px] text-gray-dark mt-[-6px]" style={{ fontFamily: 'promptSemiBold' }}>{item.place_name}</Text>
+                                        <Text className="text-[13px] text-gray-dark mt-[20px]" style={{ fontFamily: 'promptLight' }}>{dataDetail.result.place_information.detail}</Text>
+                                        <View className="flex flex-row items-center mt-[30px] gap-x-[16px]">
+                                            <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
+                                                <Image className="" source={{ uri: 'https://img.icons8.com/pastel-glyph/64/2E2E2E/shipping-location--v1.png' }}
+                                                    style={{ width: 20, height: 20 }} />
+                                            </View>
+                                            <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>{item.category_code}</Text>
+                                        </View>
+                                        <View className="flex flex-row mt-[10px] items-center gap-x-[16px]">
+                                            <View className="justify-center items-center h-[45px] w-[45px] bg-gray-light rounded-3xl">
+                                                <Image className="" source={{ uri: 'https://img.icons8.com/ios-glyphs/90/2E2E2E/map-marker.png' }}
+                                                    style={{ width: 22, height: 22 }} />
+                                            </View>
+                                            <Text className="text-[14px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>VIEW LOCATION</Text>
+                                        </View>
+                                    </View>
+                                )}
                         </View>
                     </View>
                 </View>
