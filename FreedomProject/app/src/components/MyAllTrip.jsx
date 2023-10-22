58a6fbeba30bbe4952eab850d2c0d584b5d1a0c0
BusabawanPrasertsuk <64070175@kmitl.ac.th>
2023-10-22 23:56:10 +0700

    fetch in MyDreamTrip

--- FreedomProject/app/src/components/MyAllTrip.jsx
+++ FreedomProject/app/src/components/MyAllTrip.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import {
     SafeAreaView,
     Text,
@@ -10,8 +11,14 @@ import {
     Pressable
 } from 'react-native';
 import { useFonts } from '@expo-google-fonts/prompt';
+import { firebase_auth, db } from '../firebase/firebaseConfig';
+import { query, where, doc, getDoc, getDocs, collection, addDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 
-export default function MyAllTrip({ navigation }) {
+export default function MyAllTrip({ navigation, item }) {
+    // console.log("eiei");
+    console.log(item);
+
+    const [isMenuPopup, setMenuPopup] = useState(false);
 
     const [loaded] = useFonts({
         promptLight: require("../assets/fonts/Prompt-Light.ttf"),
@@ -21,6 +28,28 @@ export default function MyAllTrip({ navigation }) {
         promptBold: require("../assets/fonts/Prompt-Bold.ttf"),
     });
 
+    function menuPopup() {
+        return (
+            <View className="bg-gray-light absolute z-20 left-12 top-9 w-auto h-auto rounded-[5px] px-3 py-1">
+                <Pressable className="p-1 flex flex-row items-center" onPress={delTrip}>
+                    <Image className="mr-2" source={{ uri: 'https://img.icons8.com/fluency-systems-regular/9A1B29/48/filled-trash.png' }}
+                        style={{ width: 15, height: 15 }} />
+                    <Text className="text-[12px] text-red" style={{ fontFamily: 'promptMedium' }}>Delete</Text>
+                </Pressable>
+            </View>
+        );
+    }
+
+    const delTrip = async () => {
+        console.log("delete eiei")
+    }
+
+    useFocusEffect(
+        React.useCallback(() => {
+            setMenuPopup(false);
+        }, [])
+    );
+
     if (!loaded) {
         return null;
     }
@@ -28,26 +57,30 @@ export default function MyAllTrip({ navigation }) {
     return (
         <View className="m-1">
             <Pressable onPress={() => {
-                navigation.navigate("TripPlan");
+                navigation.navigate("TripPlan", { tripKey: item.key });
             }}>
                 <ImageBackground className="relative h-[210px] w-[156px] justify-end items-center"
                     source={require('../assets/TripImage.png')} imageStyle={{ borderRadius: 20 }}>
-                    <Image className="absolute top-5 left-4" source={{ uri: 'https://img.icons8.com/material-outlined/96/2E2E2E/filled-like.png' }}
-                        style={{ width: 20, height: 20 }} />
-                    <Image className="absolute top-4 right-2" source={{ uri: 'https://img.icons8.com/external-regular-kawalan-studio/24/external-kebab-menu-user-interface-regular-kawalan-studio.png' }}
+                    {/* <Image className="absolute top-5 left-4" source={{ uri: 'https://img.icons8.com/material-outlined/96/2E2E2E/filled-like.png' }}
+                        style={{ width: 20, height: 20 }} /> */}
+                    <Pressable onPress={() => { setMenuPopup(!isMenuPopup) }}>
+                        <Image className="relative top-[-90px] left-[60px]" source={{ uri: 'https://img.icons8.com/external-regular-kawalan-studio/24/external-kebab-menu-user-interface-regular-kawalan-studio.png' }}
                         style={{ width: 24, height: 24 }} />
+                    </Pressable>
+                    
                     <View className="relative bg-white opacity-40 w-[145px] h-[76px] mb-2 py-2 px-3 rounded-[20px] items-center" />
                     <View className="absolute w-[110px] h-[70px] px-1">
-                        <Text className="text-[12px] text-gray-dark" style={{ fontFamily: 'promptLight' }}>Province</Text>
-                        <Text className="text-[16px] my-[-2px] text-gray-dark" style={{ fontFamily: 'promptSemiBold' }}>Trip Name</Text>
+                        {/* <Text className="text-[12px] text-gray-dark" style={{ fontFamily: 'promptLight' }}>Province</Text> */}
+                        <Text className="text-[16px] my-[-2px] text-gray-dark" style={{ fontFamily: 'promptSemiBold' }}>{item.trip_title}</Text>
                         <View className="flex flex-row mt-[3px] items-center">
                             <Image className="mr-[6px]" source={{ uri: 'https://img.icons8.com/metro/96/2E2E2E/tear-off-calendar.png' }}
                                 style={{ width: 10, height: 10 }} />
-                            <Text className="text-[8px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>8/14 - 8/20</Text>
+                            <Text className="text-[8px] text-gray-dark" style={{ fontFamily: 'promptMedium' }}>{item.trip_start_date} - {item.trip_end_date}</Text>
                         </View>
                     </View>
                 </ImageBackground>
             </Pressable>
+            {isMenuPopup && menuPopup()}
         </View>
     );
 
--- FreedomProject/app/src/screens/MyDreamTrip.jsx
+++ FreedomProject/app/src/screens/MyDreamTrip.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useFocusEffect } from '@react-navigation/native';
 import {
     Button,
     SafeAreaView,
@@ -8,18 +9,23 @@ import {
     StyleSheet,
     TextInput,
     ScrollView,
-    Pressable
+    Pressable,
+    FlatList
 } from 'react-native';
 import { useFonts } from '@expo-google-fonts/prompt';
 import { MyAllTrip, Header } from '../components/index';
 import { useSelector } from "react-redux";
 import { tripSelector } from '../redux/tripsSlice';
+import { firebase_auth, db } from '../firebase/firebaseConfig';
+import { query, where, doc, getDoc, getDocs, collection, addDoc, updateDoc, deleteDoc } from 'firebase/firestore';
 
 export default function MyDreamTrip({ navigation }) {
 
     const trip = useSelector(tripSelector);
     const tripInfo = trip.trips;
-    console.log(tripInfo);
+
+    const [myAlltrip, setMyAllTrip] = useState([]);
+    // console.log(tripInfo);
 
     const [loaded] = useFonts({
         promptLight: require("../assets/fonts/Prompt-Light.ttf"),
@@ -29,12 +35,38 @@ export default function MyDreamTrip({ navigation }) {
         promptBold: require("../assets/fonts/Prompt-Bold.ttf"),
     });
 
+    const getMyAllTrip = async () => {
+        const user = firebase_auth.currentUser;
+        console.log(user.uid);
+        try {
+            const querySnapshot = await getDocs(query(collection(db, "trips"), where("user_id", "==", user.uid)));
+            // console.log("Total Wishlist: ", querySnapshot.size);
+            const myAllTripDoc = [];
+            querySnapshot.forEach((doc) => {
+                myAllTripDoc.push({ ...doc.data(), key: doc.id });
+            });
+            console.log(myAllTripDoc)
+            setMyAllTrip(myAllTripDoc);
+            // console.log(wishlist)
+            // const isLiked = wishlistDoc.some(item_wishlist => item.place_id === item_wishlist.place_id);
+            // setLike(isLiked);
+        } catch (error) {
+            console.error("Error fetching myAllTrip:", error);
+        }
+    }
+
+    useFocusEffect(
+        React.useCallback(() => {
+            getMyAllTrip();
+        }, [])
+    );
+
     if (!loaded) {
         return null;
     }
 
     return (
-        <ScrollView className="bg-white">
+        // <ScrollView className="bg-white">
             <View className="container mx-auto bg-white">
                 <View className="h-full mx-[32px] pt-14 bg-white">
                     {/* Header */}
@@ -55,7 +87,7 @@ export default function MyDreamTrip({ navigation }) {
                     </View>
 
                     {/* SearchBar */}
-                    <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                    {/* <View style={{ flexDirection: 'row', marginTop: 10 }}>
                         <View style={[styles.SearchContainer]}>
                             <Image source={{ uri: 'https://img.icons8.com/fluency-systems-filled/48/search.png' }}
                                 style={{ width: 20, height: 20 }} className="ml-3 opacity-80" />
@@ -65,10 +97,19 @@ export default function MyDreamTrip({ navigation }) {
                             <Image source={{ uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAy0lEQVR4nO3YSw6CMBSF4X8fitHuXAawAXEBdi+ibgJjchMNYSCP2xRyvuROOijcPsgJICIiGxOAGnhZ1Ta2KifgAXS9aoEjK1Lai1+AnVVjY2fvh8eBFZxb+5/5Dw7zx1SNFM6N3FIdrcZ25dPQNdXRWvqytwMreF/bZcc+tRXwtKqsQRERyVJQat5aao4Ooc47NcfcGyn+bMQ9/U5VKjVnKCg1i4j0KZnmpFwimaYQF06mKSrOacT7f243oialXyXTHAUlUxER4esNlJFPhidtdXQAAAAASUVORK5CYII=' }}
                                 style={{ width: 20, height: 20 }} />
                         </View>
-                    </View>
+                    </View> */}
 
                     <View className="mt-2">
                         <View className="flex-row flex-wrap mt-[20px]">
+                            <FlatList
+                                data={myAlltrip}
+                                keyExtractor={item => item.place_id}
+                                renderItem={({ item }) => (
+                                    <MyAllTrip item={item} navigation={navigation} />
+                                )}
+                                numColumns={2}
+                            // showsHorizontalScrollIndicator={false}
+                            />
                             {/* <MyAllTrip navigation={navigation} />
                             <MyAllTrip navigation={navigation} />
                             <MyAllTrip navigation={navigation} />
@@ -87,7 +128,7 @@ export default function MyDreamTrip({ navigation }) {
                     </View>
                 </View>
             </View>
-        </ScrollView>
+        // </ScrollView>
     );
 }
 
